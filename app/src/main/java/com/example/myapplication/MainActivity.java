b740d18da6d1497562d58cdae1459b459de27e64
Victor <dasi0105@algonquinlive.com>
2022-09-23 15:31:11 -0400

    Lab 1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,29 +1,25 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.opengl.Visibility;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_containt);
+        Button b = (Button)findViewById(R.id.button);
+        b.setOnClickListener(this);
     }
-    TextView t = (TextView) findViewById(R.id.textView);
-    Button send = (Button) findViewById(R.id.button);
-    View.OnClickListener listener = new View.OnClickListener(){
-        @Override
-        public void onClick(View v) {
-            t.setVisibility(View.VISIBLE);
-            send.setOnClickListener(listener);
-        }
-    };
 
+    @Override
+    public void onClick(View view) {
+        TextView t = (TextView)findViewById(R.id.textView);
+        t.setVisibility(View.VISIBLE);
+
+    }
 }
