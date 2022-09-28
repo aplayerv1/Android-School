e9857af66c342f026d86d6bfcaca14868a1fb920
Victor <dasi0105@algonquinlive.com>
2022-09-28 12:31:58 -0400

    Lab 1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,89 +2,57 @@ package com.example.myapplication;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
 
 import android.annotation.SuppressLint;
 
-import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.os.Bundle;
 
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-
-
-import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Locale currentLocale;
+    public static final String EXTRA_MESSAGE =  "com.example.myapplication.MESSAGE";
+    public int i = 0;
+    public String msg;
+    public SharedPreferences perf;
+
+
+    @Override
+    protected void onPause() {
+        SharedPreferences.Editor editor = getSharedPreferences(msg, MODE_PRIVATE).edit();
+        editor.putString("welcome", msg);
+        editor.commit();
+        super.onPause();
+    }
 
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_containt);
-
-        String str = getString(R.string.snackbar);
-        String chk = getString(R.string.checked);
-
-        setContentView(R.layout.activity_main_containt);
+        setContentView(R.layout.activity_lab03);
 
         Button b = (Button)findViewById(R.id.button);
         b.setOnClickListener(this);
-        SwitchCompat sw = (SwitchCompat) findViewById(R.id.switch1);
-        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(sw.isChecked()){
 
 
-                }
-                else {
-
-                }
-            }
-        });
-
-
-        CheckBox cb = (CheckBox)findViewById(R.id.checkBox);
-        cb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton cb, boolean b) {
-                        Snackbar sn = Snackbar.make(view, chk, 5000);
-                        if (cb.isChecked()){
-                            sn.show();
-                        }
-                        sn.setAction(str,click -> cb.setChecked(!b));
-                    }
-                });
-            }
-        });
     }
 
     @Override
     public void onClick(View view) {
-        TextView t = (TextView)findViewById(R.id.textView);
-        EditText tt = (EditText)findViewById(R.id.TextInput);
-        String s = String.valueOf(tt.getText());
-        t.setText(s);
-        String st = getResources().getString(R.string.toast_fr);
-        Toast.makeText(getApplicationContext(), st, Toast.LENGTH_SHORT).show();
+        int i = 1;
+        Intent intent = new Intent(this,NameActivity.class);
+        if (i==1){
+            EditText text = (EditText)findViewById(R.id.editTextTextPersonName);
+            msg = text.getText().toString();
+            intent.putExtra(EXTRA_MESSAGE, msg);
+            startActivity(intent);
+        }
+
     }
 
 
