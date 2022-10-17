967c3e6f547db80ddf82fc2f05ec676b7b11a0dc
Victor <dasi0105@algonquinlive.com>
2022-10-17 14:42:30 -0400

    Lab 4

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,11 +4,14 @@ package com.example.myapplication;
 import static android.app.PendingIntent.getActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +20,7 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -25,6 +29,8 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity  {
     ArrayList<data> arrayList = new ArrayList<>();
     Base adapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,11 +39,21 @@ public class MainActivity extends AppCompatActivity  {
         ListView lv =(ListView)findViewById(R.id.listView);
         @SuppressLint("UseSwitchCompatOrMaterialCode") Switch sw = (Switch)findViewById(R.id.switch1);
         Button b = (Button)findViewById(R.id.button);
+        Sql sql = new Sql(MainActivity.this,2);
+       // sql.onUpgrade( sql.getDB(),4,5);
+        arrayList = sql.getTasks();
+        adapter = new Base(getApplicationContext(), arrayList);
+        lv.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+        Resources res = getResources();
+
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                arrayList.add(new data(tx.getText().toString(),sw.isChecked()));
+               // arrayList.add(new data(tx.getText().toString(),sw.isChecked()));
+                sql.addTasks(tx.getText().toString(),sw.isChecked());
+                arrayList = sql.getTasks();
                 if (arrayList!=null){
                     adapter = new Base(getApplicationContext(), arrayList);
                     lv.setAdapter(adapter);
@@ -50,11 +66,15 @@ public class MainActivity extends AppCompatActivity  {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
                 new AlertDialog.Builder(MainActivity.this).setTitle(R.string.mesg)
-                        .setMessage(R.string.row+position)
+                        .setMessage(String.format(res.getString(R.string.row), position+1))
                                 .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialogInterface, int i) {
-                                        arrayList.remove(position);
+                                        //arrayList.remove(position);
+                                        sql.removeTask(arrayList.get(position).getId());
+                                        arrayList = sql.getTasks();
+                                        adapter = new Base(getApplicationContext(), arrayList);
+                                        lv.setAdapter(adapter);
                                         adapter.notifyDataSetChanged();
                                     }
                                 }).setNegativeButton(R.string.no,null).show();
--- app/src/main/java/com/example/myapplication/data.java
+++ app/src/main/java/com/example/myapplication/data.java
@@ -1,16 +1,19 @@
 package com.example.myapplication;
 
 public class data {
-
+    private int id;
     private String name;
     private Boolean urgent;
 
 
-    public data(String name, Boolean urgent) {
+    public data(String name, Boolean urgent, int id) {
         this.name = name;
         this.urgent = urgent;
+        this.id = id;
     }
-
+    public int getId(){
+        return id;
+    }
     public String getName() {
         return name;
     }
